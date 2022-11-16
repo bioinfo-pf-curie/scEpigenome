25685d54f07404bf3214b16724d380700da987f3
LouisaHadj <louisa.hadj-abed@curie.fr>
2022-11-16 14:40:02 +0100

    add param for min reads

--- bin/stat2mqc.sh
+++ bin/stat2mqc.sh
@@ -13,7 +13,7 @@ echo "Sample_id,Sample_name,Barcoded,Index 1 and 2 found not 3,Index 1 found not
 echo "Sample_id,Sample_name,Deduplicated reads, Window duplicates,RT duplicates,PCR duplicates,Uniquely mapped not barcoded,Mapped to multiple loci,Unmapped" > scChIPseq_alignments.csv
 ## Summary table
 # The column names have to be the same as the ID column in the multiqcConfig.yaml !!!!! 
-echo -e "Sample_id,Sample_name,Tot_frag,Cells>1000reads,Reads(median)/cell,Aligned,Aligned_Barcoded,Deduplicated_reads" > scChIPseq_table.csv
+echo -e "Sample_id,Sample_name,Tot_frag,Cells>minReads,Reads(median)/cell,Aligned,Aligned_Barcoded,Deduplicated_reads" > scChIPseq_table.csv
 
 for sample in $all_samples
 do
